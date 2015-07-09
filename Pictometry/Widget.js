ffb52ddd91813f508bc7d2c893b5a3dc84b23224
kevinsagis <1234qwerASDF!@#$>
2015-07-09 11:44:14 -0400

    Update
    Can now be used for sites with any projection (i.e. state plane, web
    mercator, etc)

--- Pictometry/Widget.js
+++ Pictometry/Widget.js
@@ -8,11 +8,15 @@ define([
   "dijit/form/Button",
   "dojo/on",
   "dojo/aspect",
+  "esri/tasks/GeometryService",
+  "esri/geometry/Geometry",
+  "esri/geometry/Point",
+  "esri/tasks/ProjectParameters",
   "esri/map"
    
 
 ],
-function (declare, BaseWidget, ToggleButton, dom, registry, Button, on, aspect, map) {
+function (declare, BaseWidget, ToggleButton, dom, registry, Button, on, aspect, GeometryService, Geometry, Point, ProjectParameters, map) {
     var handlerPictometry = {advice: null};
 
     var clazz = declare([BaseWidget], 
@@ -24,13 +28,17 @@ function (declare, BaseWidget, ToggleButton, dom, registry, Button, on, aspect, 
      
  baseClass: 'jimu-widget-pictometry',
  name: 'Pictometry',
+ pt: '',
 
         _pictometryClick: function () {
 
-
             pictometryCloudURL = this.config.configText;
             map = this.map;
-         
+//         esri geometry service
+            var EsrigeometryService = new GeometryService("http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer");
+            // !!!!!!!! I recommend you set up your own geometry service and server !!!!!! //////
+
+
             //handlers 
             if (!handlerPictometry.advice) {
 
@@ -38,38 +46,47 @@ function (declare, BaseWidget, ToggleButton, dom, registry, Button, on, aspect, 
                                 map.setMapCursor("url(widgets/Pictometry/images/pictometryCursor.cur),auto");
 
                 handlerPictometry = map.on("click", function (evt) {
-
-                    var pt = esri.geometry.webMercatorToGeographic(evt.mapPoint);
+                    
+                    if (map.spatialReference.wkid === 102100 || map.spatialReference.wki === 3857 || map.spatialReference.wki === 102113) {
+                         pt = esri.geometry.webMercatorToGeographic(evt.mapPoint);
                     var url = pictometryCloudURL + 'lat=' + pt.y + '&lon=' + pt.x;
-
                     window.open(url);
+                    } else {
+                        //convert to Geographic from current map projection
+                    var outSR = new esri.SpatialReference({
+                            wkid: 4326
+                        });
+                        var inLat = evt.mapPoint.y;
+                        var inLon = evt.mapPoint.x;
+                        var inSR = map.spatialReference;
+                        var inputpoint = new Point(inLon, inLat, inSR);
+                        var PrjParams = new ProjectParameters();
+                        PrjParams.geometries = [inputpoint];
+                        PrjParams.outSR = outSR;
+                        EsrigeometryService.project(PrjParams, function (outputpoint) {
+                            pt = outputpoint[0];
+                            var url = pictometryCloudURL + 'lat=' + pt.y + '&lon=' + pt.x;
+                            window.open(url);
+                        });
 
-                    ///// remove after one click
+                    };//end of else for wkids other than web mercator
+                    /// remove after one click
                     //     handlerPictometry.remove();
                     map.setMapCursor("default");
                     Pict = false;
                     ////remove after one click
-
-
                     handlerPictometry.remove();
                     handlerPictometry.advice = null; //hm, should we disable after one click and re-wire other handlers? Revisit later..
                 });
 
             };  /// end else for handlerPictometry
 
-
             /////  end of pictometry widget
-
             /////
         },
 
         startup: function () {
             this.inherited(arguments);
-            //alert('startup');
-
-          //  alert(this.config.configText);
-
-
         },
 
         onClose:
@@ -77,12 +94,9 @@ function (declare, BaseWidget, ToggleButton, dom, registry, Button, on, aspect, 
         this.inherited(arguments);
         map = this.map;
         map.setMapCursor("default");
-
         handlerPictometry.remove();
         handlerPictometry.advice = null;
     }
-
-
     });
 
     clazz.hasStyle = true;
